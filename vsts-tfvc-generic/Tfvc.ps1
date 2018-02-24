8b8ffcf04cc59a9eb79bd358f5632af9ddc2d9f4
Jesse Houwing <jesse.houwing@gmail.com>
2018-02-24 14:00:12 +0100

    Adds generic TFVC task
    Based on Powershell3 handler
    Uses agent or Visual Studio TF.exe instance
    Drops dependency on loads of crazy powershell

---
+++ vsts-tfvc-generic/Tfvc.ps1
@@ -0,0 +1,24 @@
+﻿if (-not (Get-Module VstsTaskSdk))
+{
+    Import-Module ".\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1"
+}
+
+function Find-VisualStudioTf {
+    $ErrorActionPreference = 'Stop'
+
+    $path = .\tools\vswhere -latest -products * -requires Microsoft.VisualStudio.TeamExplorer -property installationPath
+    $path = join-path $path '\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\tf.exe'
+    return $path
+}
+
+function Find-AgentTf {
+    $ErrorActionPreference = 'Stop'
+
+    $path = Get-VstsTaskVariable -Name "Agent.HomeDirectory" -Require
+    $path = join-path $path '\externals\vstshost\tf.exe'
+    return $path
+}
+
+$tf = Find-VisualStudioTf
+
+Invoke-VstsTool -FileName $tf -Arguments "vc"
